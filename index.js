4904579d71f6efe7241fb0eadef32c147e2114d7
Madhao Parate <madhao.parate@morningstar.com>
2022-06-02 17:43:50 +0530

    file added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+//Importing express library to create server
+const express = require("express")
+
+const app = express()
+
+app.get("/user", (req, res) =>{
+    res.send("Fetch API Call")
+})
+const port = 5000
+
+app.listen(port,(req,res) =>{
+    console.log(`Starting application on port : http://localhost: ${port}`)
+})
+
